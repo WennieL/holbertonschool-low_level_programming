36fdc0f8e3f5349539d2f45ecb5d00e030e3f57a
WennieL <jecxu@hotmail.com>
2024-02-17 00:58:40 +0000

    5. Hello,printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 
 /**
- * main: use printf to print
- * with proper grammar, but the outcome is a piece of art,
+ * main - Entry point
+ *
+ * Use printf to print
+ * "with proper grammar, bit the outcome is a piece of art,"
  * followed by a new line
  *
- * Return: value 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
