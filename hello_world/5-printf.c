2b23fda40b0b6af0430747554c021ed0cc59f095
WennieL <jecxu@hotmail.com>
2024-02-17 00:45:08 +0000

    5. Hello,printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main: use printf to print
+ * with proper grammar, but the outcome is a piece of art,
+ * followed by a new line
+ *
+ * Return: value 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
