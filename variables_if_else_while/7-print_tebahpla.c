cfe3aff661566fcf531c2a7ede6a73c14e159760
WennieL <jecxu@hotmail.com>
2024-02-18 07:58:46 +0000

    7. Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Write a program that prints the lowercase alphabet
+ * in reverse, followed by a new line
+ *
+ * can only use putchar twice in the code
+ *
+ * Return: Alwasy 0 (Sucess)
+ *
+ */
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch > 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+
+	putchar('\n');
+	return (0);
+}
