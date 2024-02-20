99165b7e9c8db5ffa7359f95019ed97775600886
WennieL <jecxu@hotmail.com>
2024-02-20 04:50:32 +0000

    0. _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Write a program that prints _putchar
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
+		
