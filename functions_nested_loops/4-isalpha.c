7ffd016e5792c0417697b34c1f6a770ab679a47b
WennieL <jecxu@hotmail.com>
2024-02-20 23:50:22 +0000

    4. isalpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -7,7 +7,7 @@
  * alphabetic character (a letter)
  * @c: The character to be checked
  *
- * Return 1 if the character is an alphabetic character, 0 otherwise
+ * Return: 1 if the character is an alphabetic character, 0 otherwise
  *
  */
 
