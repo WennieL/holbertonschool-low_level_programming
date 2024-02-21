30625afbacbb5083e546f649a75a30b89b3e114a
WennieL <jecxu@hotmail.com>
2024-02-21 08:18:48 +0000

    9. timetables

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,27 +15,21 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-
 		_putchar('0');
 
-		for (j = 1; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
-
 			result = i*j;
-
 			_putchar(',');
 			_putchar(' ');
 
 			if (result < 10)
 			{
-
 				_putchar(' ');
 				_putchar(result + '0');
-
 			}
 			else 
 			{
-
 				_putchar((result / 10) + '0');
 				_putchar((result % 10) + '0');
 
