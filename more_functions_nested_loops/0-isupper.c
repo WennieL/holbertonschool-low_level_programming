811e95accaea42ebed8815f1a82d82ab85433b06
WennieL <jecxu@hotmail.com>
2024-02-23 03:19:58 +0000

    0. isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -4,16 +4,18 @@
 #include "main.h"
 
 /**
- * is_upper - checks if character is uppercase
- * @c: The character to be checked
+ * _isupper - Checks if a character is uppercase.
+ * @c: The character to be checked.
  *
- * Return: 1 if @c is uppercase, 0 otherwis
+ * Return: 1 if @c is uppercase, 0 otherwise.
  *
  * Description: This function checks whether the character @c
  * is an uppercase letter or not. It returns 1 if @c is uppercase,
- * and 0 otherwise. It is similar to the standard library function isupper()
- *
+ * and 0 otherwise. It is similar to the standard library function
+ * isupper(). For more information, refer to the man page by running
+ * 'man isupper' in the terminal.
  */
+
 
 int _isupper(int c)
 {
