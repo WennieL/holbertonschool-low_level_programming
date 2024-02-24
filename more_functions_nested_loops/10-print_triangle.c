d3f46d5f6a7d0fc045e110548bba2c69fcdf6d18
WennieL <jecxu@hotmail.com>
2024-02-24 08:44:05 +0000

    10. Triangles

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle using '#'
+ * @size: size of the triangle
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+
+
+
