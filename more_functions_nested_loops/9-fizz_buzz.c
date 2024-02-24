7400c87579920d31eb2766a14658fcec7faa2432
WennieL <jecxu@hotmail.com>
2024-02-24 04:01:30 +0000

    9. Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -16,7 +16,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3) == 0 && (i % 5) == 0)
 		{
@@ -31,12 +31,10 @@ int main(void)
 		{
 			printf("Buzz");
 		}
-		else 
-		{
+		else
 			printf("%d", i);
-		}
-
-		printf(" ");
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
