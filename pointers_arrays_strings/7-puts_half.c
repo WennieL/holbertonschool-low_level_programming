77d5e8f5c6cc13408158de39eedbde844796ac51
WennieL <jecxu@hotmail.com>
2024-02-27 22:33:38 +0000

    7. prints last few char

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - print the second half of the string
+ * @str: The string to be passed
+ *
+ * if the number of char is odd, sholud print the last n char of the string
+ * where n = (length_of_the_string - 1) / 2
+ * Return: void
+ *
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int start_index;
+
+	while (*str != 0)
+	{
+		length++;
+		str++;
+
+	}
+
+	str -= length;
+
+	if (length % 2 == 0)
+	{
+		start_index = length / 2;
+	}
+	else
+	{
+		start_index = (length - 1) / 2;
+	}
+
+	while (start_index <= length)
+	{
+		_putchar(str[start_index]);
+		start_index++;
+	}
+
+	_putchar('\n');
+}
