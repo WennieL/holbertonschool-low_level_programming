96f9f70ef10ef0c477c4917e2768bfe4fc96d61c
WennieL <jecxu@hotmail.com>
2024-02-29 03:16:03 +0000

    4. reverse n num

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reverse_array - Reverses the content of an array of integers.
+ * @a: Pointer to the array of integers.
+ * @n: Number of elements in the array.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp = 0;
+	int index = 0;
+
+	while (index < n)
+	{
+		temp = a[n];
+		a[n] = a[index];
+		a[index] = temp;
+
+		index++;
+		n--;
+	}
+}
