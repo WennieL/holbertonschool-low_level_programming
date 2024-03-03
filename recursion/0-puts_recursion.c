4dd63824e03757c41576163357ad93262ac533a1
WennieL <jecxu@hotmail.com>
2024-03-03 22:36:45 +0000

    0. puts recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -16,11 +16,11 @@
 
 void _puts_recursion(char *s)
 {
-	while (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	else
+		_putchar('\n');
 }
