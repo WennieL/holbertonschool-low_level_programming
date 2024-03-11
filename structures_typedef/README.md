59f214a04a77b6eab97930ede640ca163bc7fe1d
WennieL <jecxu@hotmail.com>
2024-03-11 03:52:09 +0000

    0. Doggy struct

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+<br> C - Structures, typedef <br> <br> 0. Poppy <br> Define a new type struct dog with the following elements:<br> name, type = char *<br> age, type = float <br> owner, type = char *<br> 
---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a structure for a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: Defines a structure representing a dog,
+ * including its name, age, and owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
