9a4f084183cc33b7470cb5f886c01c489e8ea821
WennieL <jecxu@hotmail.com>
2024-03-16 03:38:05 +0000

    3. operation

---
+++ function_pointers/3-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+#include "3-get_op_func.c"
+#include "3-op_functions.c"
+
+/**
+ */
+
+int main()
+{
+	int result;
+
+	result = get_op_func(char, 
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+/**
+ * op_add - add 2 numbers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: the sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract 2 numbers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: the difference of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply 2 numbers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: the product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide 2 numbers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: the result of the division of a by b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - the remainder of 2 numbers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: the remainder of the division of a by b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
