c26db3b35b9ccf4cab5ff8a8d83543ec7937a5ee
WennieL <109007964+WennieL@users.noreply.github.com>
2024-03-31 22:32:16 +1100

    Update 2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -20,7 +20,6 @@ list_t *add_node(list_t **head, const char *str)
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
-		free(new_node);
 		return (NULL);
 	}
 
@@ -30,6 +29,12 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	
 	new_node->len = i;
 	new_node->next = *head;
 	*head = new_node;
