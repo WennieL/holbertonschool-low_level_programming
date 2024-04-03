6ca959bd53e69a20a07bc7887bcd8d96bdfcd0aa
WennieL <jecxu@hotmail.com>
2024-04-03 08:08:29 +0000

    fix Betty style code

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -15,12 +15,12 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *current;
 	unsigned int i;
-	
+
 	if (head == NULL || *head == NULL)
 		return (-1);
 
 	current = *head;
-	
+
 	if (index == 0)
 	{
 		*head = current->next;
@@ -36,10 +36,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		current = current->next;
 		i++;
 	}
-	
+
 	if (current == NULL)
 		return (-1);
-	
+
 	current->prev->next = current->next;
 	if (current->next != NULL)
 		current->next->prev = current->prev;
