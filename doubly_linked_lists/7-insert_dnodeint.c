fc6ae7642e569b8d953fc6c925fc261c651517a0
WennieL <jecxu@hotmail.com>
2024-04-03 02:27:36 +0000

    fic bug

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node = malloc(sizeof(dlistint_t));
 
 	if (h == NULL)
-		new_node = *h;
+		return (NULL);
 
 	current = *h;
 
@@ -36,6 +36,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
+
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		if (*h != NULL)
+		{
+			(*h)->prev = new_node;
+		}
+		*h = new_node;
+		return (new_node);
+	}
 
 	new_node->prev = current->prev;
 	new_node->next = current;
