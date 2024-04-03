dca1a5c2b894e0a8d926722909d35d3d5f50b375
WennieL <jecxu@hotmail.com>
2024-04-03 02:46:44 +0000

    fix bug

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	while (current != NULL && idx != i)
 	{
+		if (i > idx)
+		{
+			printf("(nil)");
+		}
 		current = current->next;
 		i++;
 	}
