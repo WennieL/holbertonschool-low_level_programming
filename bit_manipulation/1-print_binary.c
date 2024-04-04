f31222d788841b80388bb67b72135bf975f2071b
WennieL <jecxu@hotmail.com>
2024-04-04 22:58:49 +0000

    Task 1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the input number
+ *
+ * Return: converted number to binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+
+	if (n & 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
