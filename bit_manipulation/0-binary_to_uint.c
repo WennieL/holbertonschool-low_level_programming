b6efda277e784d36b99f482c2a760d70858ead0c
WennieL <jecxu@hotmail.com>
2024-04-04 05:04:17 +0000

    Task 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -10,9 +10,10 @@
  * Description:
  * This function takes a string of characters representing a binary number
  * and converts it into an unsigned integer. It iterates through each character
- * in the string, shifting the current value of the integer left by one position
- * and adding the value of the current bit if it's '1'. If the string contains
- * characters other than '0' and '1', or if it's NULL, the function returns 0.
+ * in the string, shifting the current value of the integer left by one
+ * position and adding the value of the current bit if it's '1'.
+ * If the string contains characters other than '0' and '1', or if it's NULL,
+ * the function returns 0.
  *
  * Return: The converted unsigned int, or 0 if `b` is NULL or contains
  *         characters other than '0' and '1'.
