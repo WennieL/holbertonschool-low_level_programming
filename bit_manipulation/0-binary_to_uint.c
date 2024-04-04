93e28d6b1044c02aa81334e4e0638157467a3309
WennieL <jecxu@hotmail.com>
2024-04-04 05:00:55 +0000

    Task 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -6,6 +6,11 @@
  * binary_to_unit - coverts a binary number to an unsigned int
  * @b: pointer points to a string of 0 and 1 chars
  *
+ * Description: This header file contains a function binary_to_uint
+ * that converts a binary number represented as a string of
+ * '0' and '1' characters to an unsigned integer.
+ * The function takes a pointer b pointing to the binary string as input
+ *
  * Return: the converted number, or 0 if
  * there is one or more chars in the string b that is not 0 or 1
  * b is Null
