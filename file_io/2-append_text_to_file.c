ce1f09d6530288a8f6083bad1cd2613357c3ed02
WennieL <jecxu@hotmail.com>
2024-04-05 10:48:15 +0000

    Task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	/* 0644 = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH */
-	fp = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	fp = open(filename, O_WRONLY | O_APPEND, 0644);
 	if (fp == -1)
 		return (-1);
 
